2820e23928d9cc9808077be81704be7692791ad5
Kolya <kolya.romanov8@gmail.com>
2020-12-01 11:17:15 +0200

    AddedSynchronization

--- src/test/java/csvParser/MnogoPotokTest.java
+++ src/test/java/csvParser/MnogoPotokTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -13,11 +14,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class MnogoPotokTest {
 
     @Test
-    void process() throws FileNotFoundException {
+    void process() throws IOException {
         String expected="2+2+0+6";
         List<String> line=(Arrays.asList("10", "AU", "", "Au\"stralia"));
         MnogoPotok concurrentParser = new MnogoPotok();
-        concurrentParser.process(8, line, "+", ',',  "src/main/resources/test");
+        concurrentParser.process(8, line, "+", ',');
         File file = new File("result.txt");
         Scanner scan= new Scanner(file);
         assertEquals(expected, scan.nextLine());
